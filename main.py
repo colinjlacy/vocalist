e2e20a6411e27af474d1f8b74683d6cd887d1d76
Colin Lacy <colinjlacy@gmail.com>
2019-06-17 06:10:56 -0500

    parses speech from microphone

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import speech_recognition as sr
-import os
 
-# TODO: set up strategy abstraction
-def setup_recognizer(strategy="sphinx"):
+
+def setup_recognizer():
     r = sr.Recognizer()
     return r
 
 
-def fetch_audio_file(audio_recognizer, path="audio-files/harvard.wav"):
-    rel_path = os.path.join(os.getcwd(), path)
-    src = sr.AudioFile(rel_path)
+def fetch_audio(audio_recognizer):
+    src = sr.Microphone()
     with src as source:
-        audio_file = audio_recognizer.record(source)
+        # audio_recognizer.adjust_for_ambient_noise(source)
+        audio_file = audio_recognizer.listen(source)
     return audio_file
 
 
 if __name__ == '__main__':
     print(sr.__version__)
     recognizer = setup_recognizer()
-    audio = fetch_audio_file(recognizer)
+    audio = fetch_audio(recognizer)
+    # TODO: set up strategy abstraction
     text = recognizer.recognize_google(audio)
     print(text)
