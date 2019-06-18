748fc805986e3e4c649f5de42418b88ade0c726d
Colin Lacy <colinjlacy@gmail.com>
2019-06-18 05:57:55 -0500

    refactors into a class, adds service abstraction

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
 import speech_recognition as sr
 import os
 
+STRATEGY_MAP = {
+    "sphinx": "recognize_sphinx",
+    "bing": "recognize_bing",
+    "google": "recognize_google",
+    "google_cloud": "recognize_google_cloud",
+    "houndify": "recognize_houndify",
+    "ibm": "recognize_ibm",
+    "wit": "recognize_wit"
+}
 
-def setup_recognizer():
-    r = sr.Recognizer()
-    return r
-
-
-def fetch_audio(audio_recognizer):
-    # requires C-lib PortAudio, more info here: https://realpython.com/python-speech-recognition/#installing-pyaudio
-    src = sr.Microphone()
-    with src as source:
-        # audio_recognizer.adjust_for_ambient_noise(source)
-        audio_file = audio_recognizer.listen(source)
-    return audio_file
+STRATEGY_LIST = ('sphinx', 'bing', 'google', 'google_cloud', 'houndify', 'ibm', 'wit')
 
 
-def recognize_audio(audio):
-    try:
-        # TODO: set up strategy abstraction
-        return recognizer.recognize_google(audio)
-    except sr.UnknownValueError:
-        return None
+class Listener:
 
+    def __init__(self, strategy="google", credentials=None):
+        self.AVAILABLE_STRATEGIES = STRATEGY_LIST
+        self.__set_strategy(strategy)
+        self.credentials = credentials
+        self.mic = sr.Microphone()
 
-def print_speech(speech_text):
-    try:
-        assert speech_text is not None
-        print(speech_text)
-    except AssertionError:
-        notify_error("There was a problem", "Could not process your speech into text")
+    def __set_strategy(self, chosen_strategy):
+        self.recognizer = sr.Recognizer()
+        self.transcribe = self.recognizer.__getattribute__(STRATEGY_MAP[chosen_strategy])
 
+    def __activate(self):
+        with self.mic as source:
+            audio_input = self.recognizer.listen(source)
+        return audio_input
 
-def notify_error(title, err_text):
-    os.system("""
-    osascript -e 'display notification "{}" with title "{}"'
-    """.format(err_text, title))
+    def __parse(self, audio_input):
+        try:
+            return self.transcribe(audio_input)
+        except sr.UnknownValueError:
+            return None
+
+    def __print(self, speech_text):
+        try:
+            assert speech_text is not None
+            print(speech_text)
+        except AssertionError:
+            self.__notify_error("There was a problem", "Could not process your speech into text")
+
+    def __notify_error(self, title, err_text):
+        os.system("""
+        osascript -e 'display notification "{}" with title "{}"'
+        """.format(err_text, title))
+
+    def listen(self):
+        audio = self.__activate()
+        text = self.__parse(audio)
+        self.__print(text)
 
 
 if __name__ == '__main__':
-    print(sr.__version__)
-    recognizer = setup_recognizer()
-    audio = fetch_audio(recognizer)
-    text = recognize_audio(audio)
-    print_speech(text)
+    l = Listener()
+    l.listen()
