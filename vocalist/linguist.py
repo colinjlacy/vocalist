75e6c7956a694d7342dd85932a0e3e500dd4f662
Colin Lacy <colinjlacy@gmail.com>
2019-07-02 21:01:20 -0500

    modifies to work with imported domain emissary instead of import by rel path

--- vocalist/linguist.py
+++ vocalist/linguist.py
@@ -1,13 +1,14 @@
 from queue import Queue
 from threading import Thread
-from ..arbitrator import app as nlp
+from code_domain_emissary.handlers import app as nlp
+
 
 class Linguist:
 
     def __init__(self, q):
         self.__input_q = q
         self.output_q = Queue()
-        self.__translation_thread = Thread(target=self.input_q)
+        self.__translation_thread = Thread(target=self.__input_q)
         self.__translation_thread.setDaemon(True)
         self.__run = False
 
